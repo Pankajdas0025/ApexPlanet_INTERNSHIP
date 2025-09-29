b2fb118a2184f3035980ff62d56411249c1ed746
PANKAJ KUMAR DAS <pankajkumardas8678@gmail.com>
2025-09-29 14:39:33 +0530

    Update db.php

--- BlogScript/db.php
+++ BlogScript/db.php
@@ -1,10 +1,8 @@
 <?php
 $conn = new mysqli("localhost", "root", "", "blog");  //  for localhost 
-// $conn = new mysqli("sql200.infinityfree.com ", "if0_39302216", "ofMb9nxqljF", "if0_39302216_blog ");
-
+// $conn = new mysqli("sql200.infinityfree.com ", "if0_39302216", "ofMb9nxqljF", "if0_39302216_blog ")
 
 if ($conn->connect_error) 
-{
-    die("Connection failed: " . $conn->connect_error);
+ die("Connection failed: " . $conn->connect_error);
 }
 ?>
